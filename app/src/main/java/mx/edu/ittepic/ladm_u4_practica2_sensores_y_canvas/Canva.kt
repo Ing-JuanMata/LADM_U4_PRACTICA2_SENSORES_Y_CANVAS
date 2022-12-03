806e8c99db39c9ff4564be7fe1daa6256a833a18
Juan Jesus Mata Solis <jujemataso@ittepic.edu.mx>
2022-12-03 14:37:13 -0700

    Giroscopio

--- app/src/main/java/mx/edu/ittepic/ladm_u4_practica2_sensores_y_canvas/Canva.kt
+++ app/src/main/java/mx/edu/ittepic/ladm_u4_practica2_sensores_y_canvas/Canva.kt
@@ -7,6 +7,7 @@ import android.graphics.Paint
 import android.view.View
 
 class Canva(p: MainActivity) : View(p) {
+    val main = p
     val bruja = BitmapFactory.decodeResource(resources, R.drawable.bruja)
     val luna = BitmapFactory.decodeResource(resources, R.drawable.luna)
     val sol = BitmapFactory.decodeResource(resources, R.drawable.sol)
@@ -16,11 +17,9 @@ class Canva(p: MainActivity) : View(p) {
     val azul = "#ADD8E6"
     val azulOscuro = "#252850"
 
-    var noche = true
-
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        if (noche) {
+        if (main.noche) {
             noche(canvas)
         } else {
             dia(canvas)
@@ -29,15 +28,39 @@ class Canva(p: MainActivity) : View(p) {
 
     private fun dia(canvas: Canvas) {
         canvas.drawColor(Color.parseColor(azul))
-        canvas.drawBitmap(sol, width / 2f - sol.width / 2f, height / 2f - sol.height / 2f, p)
-        canvas.drawBitmap(nubes, width / 2f - nubes.width / 2f, 0f, p)
-        canvas.drawBitmap(bruja, width / 2f - bruja.width / 2f, height / 2f - bruja.height / 2f, p)
+        val gx = main.gx * 100
+        val gy = main.gy * 100
+        canvas.drawBitmap(
+            sol,
+            width / 2f - sol.width / 2f + gy,
+            height / 2f - sol.height / 2f + gx,
+            p
+        )
+        canvas.drawBitmap(nubes, width / 2f - nubes.width / 2f + gy, gx, p)
+        canvas.drawBitmap(
+            bruja,
+            width / 2f - bruja.width / 2f + gy,
+            height / 2f - bruja.height / 2f + gx,
+            p
+        )
     }
 
     private fun noche(canvas: Canvas) {
         canvas.drawColor(Color.parseColor(azulOscuro))
-        canvas.drawBitmap(luna, width / 2f - luna.width / 2f, height / 2f - luna.height / 2f, p)
-        canvas.drawBitmap(nubes, width / 2f - nubes.width / 2f, 0f, p)
-        canvas.drawBitmap(bruja, width / 2f - bruja.width / 2f, height / 2f - bruja.height / 2f, p)
+        val gx = main.gx * 100
+        val gy = main.gy * 100
+        canvas.drawBitmap(
+            luna,
+            width / 2f - luna.width / 2f + gy,
+            height / 2f - luna.height / 2f + gx,
+            p
+        )
+        canvas.drawBitmap(nubes, width / 2f - nubes.width / 2f + gy, gx, p)
+        canvas.drawBitmap(
+            bruja,
+            width / 2f - bruja.width / 2f + gy,
+            height / 2f - bruja.height / 2f + gx,
+            p
+        )
     }
 }
--- app/src/main/java/mx/edu/ittepic/ladm_u4_practica2_sensores_y_canvas/MainActivity.kt
+++ app/src/main/java/mx/edu/ittepic/ladm_u4_practica2_sensores_y_canvas/MainActivity.kt
@@ -1,11 +1,86 @@
 package mx.edu.ittepic.ladm_u4_practica2_sensores_y_canvas
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import kotlin.math.cos
+import kotlin.math.sin
+import kotlin.math.sqrt
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
+    private lateinit var sensorManager: SensorManager
+    private lateinit var mProximity: Sensor
+    private lateinit var giroscope: Sensor
+    private lateinit var canva: Canva
+
+    var noche = true
+    var iniciado = false
+    var gx = 0f
+    var gy = 0f
+    var gz = 0f
+
+    private val NS2S = 1.0f / 1000000000.0f
+    private val deltaRotationVector = FloatArray(4) { 0f }
+    private var timestamp: Float = 0f
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(Canva(this))
+        canva = Canva(this)
+        setContentView(canva)
+        iniciado = true
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        mProximity = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY)
+        giroscope = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE)
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
+        // Do something here if sensor accuracy changes.
+    }
+
+    override fun onSensorChanged(event: SensorEvent) {
+        if (event.sensor.stringType.equals(giroscope.stringType)) logicaGiroscopio(
+            event.values,
+            event.timestamp
+        )
+        if (event.sensor.stringType.equals(mProximity.stringType)) logicaProximidad(event.values)
+    }
+
+    private fun logicaProximidad(valores: FloatArray) {
+        val distance = valores[0]
+
+        noche = distance < 5
+        if (iniciado) canva.invalidate()
+    }
+
+    private fun logicaGiroscopio(valores: FloatArray, evtTimestamp: Long) {
+        gx = valores[0]
+        gy = valores[1]
+        gz = valores[2]
+        if (iniciado) canva.invalidate()
+    }
+
+    override fun onResume() {
+        // Register a listener for the sensor.
+        super.onResume()
+
+        mProximity.also { proximity ->
+            sensorManager.registerListener(this, proximity, SensorManager.SENSOR_DELAY_NORMAL)
+        }
+        giroscope.also { giro ->
+            sensorManager.registerListener(this, giro, SensorManager.SENSOR_DELAY_NORMAL)
+        }
+    }
+
+    override fun onPause() {
+        // Be sure to unregister the sensor when the activity pauses.
+        super.onPause()
+        sensorManager.unregisterListener(this)
     }
 }
